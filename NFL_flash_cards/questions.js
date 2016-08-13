424f040181533022f76871879e90dc8220c7f366
Alex W <wapniarski@gmail.com>
2016-08-13 16:41:14 -0700

    added nfl flash cards and america flash cards

---
+++ NFL_flash_cards/questions.js
@@ -0,0 +1,67 @@
+var questions = [
+{"What city do the Forty Niners play in?": [
+	'San Francisco']},
+{"What city do the Bears play in?": [
+	'Chicago']},
+{"What city do the Bengals play in?": [
+'Cincinnati']},
+{"What city do the Bills play in?": [
+'Buffalo']},
+{"What city do the Broncos play in?": [
+'Denver']},
+{"What city do the Browns play in?": [
+'Cleveland']},
+{"What city do the Buccaneers play in?": [
+'Tampa Bay']},
+{"What city do the Cardinals play in?": [
+'Arizona']},
+{"What city do the Chargers play in?": [
+'San Diego']},
+{"What city do the Chiefs play in?": [
+'Kansas City']},
+{"What city do the Colts play in?": [
+'Indianapolis']},
+{"What city do the Cowboys play in?": [
+'Dallas']},
+{"What city do the Dolphins play in?": [
+'Miami']},
+{"What city do the Eagles play in?": [
+'Philadelphia']},
+{"What city do the Falcons play in?": [
+'Atlanta']},
+{"What city do the Giants play in?": [
+'New York']},
+{"What city do the Jaguars play in?": [
+'Jacksonville']},
+{"What city do the Jets play in?": [
+'New York']},
+{"What city do the Lions play in?": [
+'Detroit']},
+{"What city do the Packers play in?": [
+'Green Bay']},
+{"What city do the Panthers play in?": [
+'Carolina']},
+{"What city do the Patriots play in?": [
+'New England']},
+{"What city do the Raiders play in?": [
+'Oakland']},
+{"What city do the Rams play in?": [
+'Saint Louis']},
+{"What city do the Ravens play in?": [
+'Baltimore']},
+{"What city do the Redskins play in?": [
+	'Washington']},
+{"What city do the Saints play in?": [
+	'New Orleans']},
+{"What city do the Seahawks play in?": [
+	'Seattle']},
+{"What city do the Steelers play in?": [
+	'Pittsburgh']},
+{"What city do the Texans play in?": [
+	'Houston']},
+{"What city do the Titans play in?": [
+	'Tennesse']},
+{"What city do the Vikings play in?": [
+	'Minnesota']},
+];
+
---
+++ america_flash_cards/questions.js
@@ -0,0 +1,100 @@
+var questions = [
+{"Where is Argentina?": [ 
+	'South America']},
+{"Where is Bolivia?": [ 
+	'South America']},
+{"Where is Brazil?": [ 
+	'South America']},
+{"Where is Chile?": [ 
+	'South America']},
+{"Where is Colombia?": [ 
+	'South America']},
+{"Where is Ecuador?": [ 
+	'South America']},
+{"Where is Guyana?": [ 
+	'South America']},
+{"Where is Paraguay?": [ 
+	'South America']},
+{"Where is Peru?": [ 
+	'South America']},
+{"Where is Suriname?": [ 
+	'South America']},
+{"Where is Uruguay?": [ 
+	'South America']},
+{"Where is Venezuela?": [ 
+	'South America']},
+{"Where is Anguilla?": [
+	'North America']},
+{"Where is Antigua and Barbuda?": [
+	'North America']},
+{"Where is Aruba?": [
+	'North America']},
+{"Where is Bahamas?": [
+	'North America']},
+{"Where is Barbados?": [
+	'North America']},
+{"Where is Belize?": [
+	'North America']},
+{"Where is Bermuda?": [
+	'North America']},
+{"Where is The British Virgin Islands?": [
+	'North America']},
+{"Where is Canada?": [
+	'North America']},
+{"Where is The Cayman Islands?": [
+	'North America']},
+{"Where is Costa Rica?": [
+	'North America']},
+{"Where is Cuba?": [
+	'North America']},
+{"Where is Dominica?": [
+	'North America']},
+{"Where is Dominican Republic?": [
+	'North America']},
+{"Where is El Salvador?": [
+	'North America']},
+{"Where is Greenland?": [
+	'North America']},
+{"Where is Grenada?": [
+	'North America']},
+{"Where is Guadeloupe?": [
+	'North America']},
+{"Where is Guatemala?": [
+	'North America']},
+{"Where is Haiti?": [
+	'North America']},
+{"Where is Honduras?": [
+	'North America']},
+{"Where is Jamaica?": [
+	'North America']},
+{"Where is Martinique?": [
+	'North America']},
+{"Where is Mexico?": [
+	'North America']},
+{"Where is Montserrat?": [
+	'North America']},
+{"Where is Netherlands Antilles?": [
+	'North America']},
+{"Where is Nicaragua?": [
+	'North America']},
+{"Where is Panama?": [
+	'North America']},
+{"Where is Puerto Rico?": [
+	'North America']},
+{"Where is Saint Barthélemy?": [
+	'North America']},
+{"Where is Saint Kitts and Nevis?": [
+	'North America']},
+{"Where is Saint Lucia?": [
+	'North America']},
+{"Where is Saint Martin?": [
+	'North America']},
+{"Where is Saint Pierre and Miquelon?": [
+	'North America']},
+{"Where is Saint Vincent and the Grenadines?": [
+	'North America']},
+{"Where is Trinidad and Tobago?": [
+	'North America']},
+{"Where is The United States?": [
+	'North America']},
+];
