b8670c4c7031c7a86622ccdb0115f2825b19b82a
Alex W <wapniarski@gmail.com>
2016-08-13 15:19:45 -0700

    added state flash cards skill

---
+++ state_flash_cards/questions.js
@@ -0,0 +1,102 @@
+ var questions = [
+	{"What is A L?": [
+		'Alabama']},
+	{"What is A K?": [
+		'Alaska']},
+	{"What is A Z?": [
+		'Arizona']},
+	{"What is A R?": [
+		'Arkansas']},
+	{"What is C A?": [
+		'California']},
+	{"What is C O?": [
+		'Colorado']},
+	{"What is C T?": [
+		'Connecticut']},
+	{"What is D E?": [
+		'Delaware']},
+	{"What is F L?": [
+		'Florida']},
+	{"What is G A?": [
+		'Georgia']},
+	{"What is H I?": [
+		'Hawaii']},
+	{"What is I D?": [
+		'Idaho']},
+	{"What is I L?": [
+		'Illinois']},
+	{"What is I N?": [
+		'Indiana']},
+	{"What is I A?": [
+		'Iowa']},
+	{"What is K S?": [
+		'Kansas']},
+	{"What is K Y?": [
+		'Kentucky']},
+	{"What is L A?": [
+		'Louisiana']},
+	{"What is M E?": [
+		'Maine']},
+	{"What is M D?": [
+		'Maryland']},
+	{"What is M A?": [
+		'Massachusetts']},
+	{"What is M I?": [
+		'Michigan']},
+	{"What is M N?": [
+		'Minnesota']},
+	{"What is M S?": [
+		'Mississippi']},
+	{"What is M O?": [
+		'Missouri']},
+	{"What is M T?": [
+		'Montana']},
+	{"What is N E?": [
+		'Nebraska']},
+	{"What is N V?": [
+		'Nevada']},
+	{"What is N H?": [
+		'New Hampshire']},
+	{"What is N J?": [
+		'New Jersey']},
+ 	{"What is N M?": [
+ 		'New Mexico']},
+ 	{"What is N Y?": [
+ 		'New York']},
+  {"What is N C?": [
+  	'North Carolina']},
+ 	{"What is N D?": [
+ 		'North Dakota']},
+	{"What is O H?": [
+		'Ohio']},
+	{"What is O K?": [
+		'Oklahoma']},
+	{"What is O R?": [
+		'Oregon']},
+	{"What is P A?": [
+		'Pennsylvania']},
+ 	{"What is R I?": [
+ 		'Rhode Island']},
+ 	{"What is S C?": [
+ 		'South Carolina']},
+ 	{"What is S D?": [
+ 		'South Dakota']},
+	{"What is T N?": [
+		'Tennessee']},
+	{"What is T X?": [
+		'Texas']},
+	{"What is U T?": [
+		'Utah']},
+	{"What is V T?": [
+		'Vermont']},
+	{"What is V A?": [
+		'Virginia']},
+	{"What is W A?": [
+		'Washington']},
+ 	{"What is W V?": [
+ 		'West Virginia']},
+	{"What is W I?": [
+		'Wisconsin' ]},
+	{"What is W Y?": [
+		'Wyoming' ]}
+];
