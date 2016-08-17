936099c8b30492c231aa9df1febf21e7227c7687
Alex W <wapniarski@gmail.com>
2016-08-17 16:27:06 -0700

    added fact skills for nfl,l, america, and states

---
+++ fan_NFL_facts/src/index.js
@@ -0,0 +1,141 @@
+/**
+    Copyright 2014-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+    Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+
+        http://aws.amazon.com/apache2.0/
+
+    or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+*/
+
+/**
+ * This simple sample has no external dependencies or session management, and shows the most basic
+ * example of how to create a Lambda function for handling Alexa Skill requests.
+ *
+ * Examples:
+ * One-shot model:
+ *  User: "Alexa, ask n f l Geek for an n f l fact"
+ *  Alexa: "Here's your n f l fact: ..."
+ */
+
+/**
+ * App ID for the skill
+ */
+var APP_ID = undefined; //OPTIONAL: replace with "amzn1.echo-sdk-ams.app.[your-unique-value-here]";
+
+/**
+ * Array containing n f l facts.
+ */
+var FACTS = [
+    'Arizona Cardinals ',
+    'Atlanta Falcons ',
+    'Baltimore Ravens ',
+    'Buffalo Bills ',
+    'Carolina Panthers ',
+    'Chicago Bears ',
+    'Cincinnati Bengals ',
+    'Cleveland Browns ',
+    'Dallas Cowboys ',
+    'Denver Broncos ',
+    'Detroit Lions ',
+    'Green Bay Packers ',
+    'Houston Texans ',
+    'Indianapolis Colts ',
+    'Jacksonville Jaguars ',
+    'Kansas City Chiefs ',
+    'Miami Dolphins ',
+    'Minnesota Vikings ',
+    'New England Patriots ',
+    'New Orleans Saints ',
+    'New York Giants ',
+    'New York Jets ',
+    'Oakland Raiders ',
+    'Philadelphia Eagles ',
+    'Pittsburgh Steelers ',
+    'San Diego Chargers ',
+    'San Francisco forty niners ',
+    'Seattle Seahawks ',
+    'Saint Louis Rams ',
+    'Tampa Bay Buccaneers ',
+    'Tennessee Titans ',
+    'Washington Redskins',
+];
+
+/**
+ * The AlexaSkill prototype and helper functions
+ */
+var AlexaSkill = require('./AlexaSkill');
+
+/**
+ * SpaceGeek is a child of AlexaSkill.
+ * To read more about inheritance in JavaScript, see the link below.
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Introduction_to_Object-Oriented_JavaScript#Inheritance
+ */
+var Fact = function () {
+    AlexaSkill.call(this, APP_ID);
+};
+
+// Extend AlexaSkill
+Fact.prototype = Object.create(AlexaSkill.prototype);
+Fact.prototype.constructor = Fact;
+
+Fact.prototype.eventHandlers.onSessionStarted = function (sessionStartedRequest, session) {
+    //console.log("onSessionStarted requestId: " + sessionStartedRequest.requestId + ", sessionId: " + session.sessionId);
+    // any initialization logic goes here
+};
+
+Fact.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
+    //console.log("onLaunch requestId: " + launchRequest.requestId + ", sessionId: " + session.sessionId);
+    handleNewFactRequest(response);
+};
+
+/**
+ * Overridden to show that a subclass can override this function to teardown session state.
+ */
+Fact.prototype.eventHandlers.onSessionEnded = function (sessionEndedRequest, session) {
+    //console.log("onSessionEnded requestId: " + sessionEndedRequest.requestId + ", sessionId: " + session.sessionId);
+    // any cleanup logic goes here
+};
+
+Fact.prototype.intentHandlers = {
+    "GetNewFactIntent": function (intent, session, response) {
+        handleNewFactRequest(response);
+    },
+
+    "AMAZON.HelpIntent": function (intent, session, response) {
+        response.ask("You can say tell me an n. f. l. team, or, you can say exit... What can I help you with?", "What can I help you with?");
+    },
+
+    "AMAZON.StopIntent": function (intent, session, response) {
+        var speechOutput = "Goodbye";
+        response.tell(speechOutput);
+    },
+
+    "AMAZON.CancelIntent": function (intent, session, response) {
+        var speechOutput = "Goodbye";
+        response.tell(speechOutput);
+    }
+};
+
+/**
+ * Gets a random new fact from the list and returns to the user.
+ */
+function handleNewFactRequest(response) {
+    // Get a random n f l team from the teams list
+    var factIndex = Math.floor(Math.random() * FACTS.length);
+    var randomFact = FACTS[factIndex];
+
+    // Create speech output
+    var speechOutput = "Here's your n. f. l. team: " + randomFact;
+    var cardTitle = "Your Team";
+    response.tellWithCard(speechOutput, cardTitle, speechOutput);
+}
+
+// Create the handler that responds to the Alexa Request.
+exports.handler = function (event, context) {
+    // Create an instance of the SpaceGeek skill.
+    var fact = new Fact();
+    fact.execute(event, context);
+};
+
